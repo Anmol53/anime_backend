2118c26d5341af9132fb493b1b2b7c4d1cce6488
Anmol Agrawal <anmol.ag53@gmail.com>
2021-10-07 17:58:29 +0530

    Add files via upload

---
+++ src/db.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const mongoURI =
+  "mongodb+srv://anmol:cLgnWGIybF1ttyTn@cluster0.hh1mz.mongodb.net/Anime?retryWrites=true&w=majority";
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connection established with mongodb server 🤗");
+  })
+  .catch((err) => {
+    console.log("Error while connection 😑", err);
+  });
+
+const reviewSchema = new mongoose.Schema({
+  description: String,
+  rating: Number,
+  creationTime: Date,
+  anime_id: String,
+  user_id: mongoose.Schema.Types.ObjectId
+});
+
+const userSchema = new mongoose.Schema({
+  user_name: String,
+  first_name: String,
+  last_name: String,
+  user_mail: String,
+  password: String
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports.Review = Review;
+module.exports.User = User;
---
+++ src/index.js
@@ -0,0 +1,242 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const db = require("./db");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  session({
+    secret: "cvszcoki8rk8667o44r378",
+    cookie: {
+      maxAge: 1 * 60 * 60 * 1000 // 1 hour session
+    }
+  })
+);
+
+const { Review, User } = db;
+
+const SALT = 8;
+
+const isNullOrUndefined = (inp) => inp === null || inp === undefined;
+
+const AuthMW = async (req, res, next) => {
+  if (
+    isNullOrUndefined(req.session) ||
+    isNullOrUndefined(req.session.user_id)
+  ) {
+    res.status(401).send({
+      status: "Unauthorized",
+      message: "Not logged in"
+    });
+  } else {
+    next();
+  }
+};
+
+app.post("/signup", async (req, res) => {
+  const { user_name, password } = req.body;
+  if (
+    isNullOrUndefined(user_name) ||
+    user_name.length < 1 ||
+    isNullOrUndefined(password) ||
+    password.length < 1
+  ) {
+    res.status(400).send({
+      status: "Bad Request",
+      message: `User name and password are required fields`
+    });
+    return;
+  }
+  const existingUser = await User.findOne({ user_name });
+  if (existingUser) {
+    res.status(400).send({
+      status: "Bad Request",
+      message: `User name ${req.body.user_name} already exist!\nPlease choose another.`
+    });
+  } else {
+    const hashedPassword = bcrypt.hashSync(req.body.password, SALT);
+    const newUser = new User({
+      ...req.body,
+      password: hashedPassword
+    });
+    req.session.user_id = newUser._id;
+    newUser.save();
+    res.status(201).send({
+      status: "Created",
+      message: `User ${req.body.user_name} created!`
+    });
+    a.send();
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { user_name, password } = req.body;
+  if (isNullOrUndefined(user_name) || isNullOrUndefined(password)) {
+    res.status(400).send({
+      status: "Bad Request",
+      message: `User name and password are required fields`
+    });
+    return;
+  }
+  const existingUser = await User.findOne({ user_name });
+  if (existingUser) {
+    if (bcrypt.compareSync(req.body.password, existingUser.password)) {
+      req.session.user_id = existingUser._id;
+      res.status(200).send({
+        status: "OK",
+        message: `${req.body.user_name} Logged in!`
+      });
+    } else {
+      res.status(401).send({
+        status: "Unauthorized",
+        message: `Incorrect Password`
+      });
+    }
+  } else {
+    res.status(401).send({
+      status: "Unauthorized",
+      message: `User name ${req.body.user_name} not found`
+    });
+  }
+});
+
+app.get("/logout", async (req, res) => {
+  if (req.session) {
+    req.session.destroy(() => {
+      res.status(200).send({
+        status: "OK",
+        message: "Logged out"
+      });
+    });
+  } else {
+    res.status(400).send({
+      status: "Bad Request",
+      message: "No User Logged in"
+    });
+  }
+});
+
+app.get("/userDetails", AuthMW, async (req, res) => {
+  const existingUser = await User.findById(req.session.user_id);
+  res.status(200).send({
+    status: "OK",
+    message: `Fetched already Logged user!`,
+    user: {
+      user_name: existingUser.user_name,
+      first_name: existingUser.first_name,
+      last_name: existingUser.last_name,
+      user_mail: existingUser.user_mail
+    }
+  });
+});
+// Create
+app.post("/review", AuthMW, async (req, res) => {
+  const newReview = new Review({
+    description: req.body.description,
+    rating: req.body.rating,
+    creationTime: new Date(),
+    anime_id: req.body.anime_id,
+    user_id: req.session.user_id
+  });
+  try {
+    await newReview.save();
+    res.status(200).send({
+      status: "ok",
+      message: "Review saved",
+      review: newReview
+    });
+  } catch (e) {
+    res.status(500).send({
+      status: "Internal Server Error",
+      message: "The server has encountered an error."
+    });
+  }
+});
+
+// Update
+app.put("/todo/:reviewId", AuthMW, async (req, res) => {
+  const reviewId = req.params.reviewId;
+  try {
+    const review = await Review.findOne({
+      _id: reviewId,
+      user_id: req.session.user_id
+    });
+    if (isNullOrUndefined(review)) {
+      res.status(404).send({
+        status: "Not Found",
+        message: `Review Not Found`
+      });
+      return;
+    }
+    if (req.body.description) {
+      review.description = req.body.description;
+    }
+    if (req.body.rating) {
+      review.rating = req.body.rating;
+    }
+    await review.save();
+    res.status(200).send({
+      status: "ok",
+      message: `Successfully Updated`,
+      review: review
+    });
+  } catch (e) {
+    res.status(500).send({
+      status: "Internal Server Error",
+      message: `The server has encountered an error. ${e}`
+    });
+  }
+});
+
+// Delete
+app.delete("/todo/:reviewId", AuthMW, async (req, res) => {
+  const reviewId = req.params.reviewId;
+  try {
+    await Review.deleteOne({
+      _id: reviewId,
+      user_id: req.session.user_id
+    });
+
+    res.status(200).send({
+      status: "ok",
+      message: `Successfully Deleted`
+    });
+  } catch (e) {
+    res.status(500).send({
+      status: "Internal Server Error",
+      message: "The server has encountered an error."
+    });
+  }
+});
+
+// Sending all the todos as an Array
+app.get("/anime", AuthMW, async (req, res) => {
+  try {
+    const reviews = await Review.find({ anime_id: req.body.anime_id });
+    reviews.forEach(async (review) => {
+      const user = await User.findById(reviews.user_id);
+      review.user_name = user.user_name;
+    });
+    res.status(200).send({
+      status: "ok",
+      message: `${reviews.length} reviews fetched`,
+      reviews
+    });
+  } catch (e) {
+    res.status(500).send({
+      status: "Internal Server Error",
+      message: "The server has encountered an error."
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running at port ${process.env.PORT}`)
+);
